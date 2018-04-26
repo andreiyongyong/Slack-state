49b9c1504c57728e38ae9dee1ba9cabeba65c6e9
aramarustamyan <aramarustamyan6@gmail.com>
2018-04-26 12:25:42 +0400

    make forums style

--- resources/views/forummaster/show.blade.php
+++ resources/views/forummaster/show.blade.php
@@ -4,17 +4,18 @@
   <div class="col-sm-6"></div>
   <div class="col-sm-6"></div>
 </div> 
-  <div class="row clearfix">
-      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+  <div class="row">
+      <div class="forum-block">
           @foreach($forums as $forum)
-              <div class="col-lg-3"><img width="80" height="80" class="img-circle" src="{{ URL::to('/') }}/image/{{$forum->user['image']}}"></div>
-              <div class="col-lg-9">
-                  <span>{{$forum->user['firstname'].' '.$forum->user['lastname'] }}</span>
-                  <span>{{$forum->reply_time}}</span>
-                  <p>{{$forum->answer}}</p>
+              <div class="table-div">
+                  <div class="table-cell w-100-px"><img width="80" height="80" class="img-circle" src="{{ URL::to('/') }}/image/{{$forum->user['image']}}"></div>
+                  <div class="table-cell info-div">
+                      <span class="first-name">{{$forum->user['firstname'].' '.$forum->user['lastname'] }}</span>
+                      <span class="reply-time">{{$forum->reply_time}}</span>
+                      <p class="info-txt">{{$forum->answer}}</p>
+                  </div>
               </div>
-
           @endforeach
-      </div> 
+      </div>
   </div> 
 @endsection

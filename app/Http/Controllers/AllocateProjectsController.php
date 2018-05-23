450af4e6ecc2cfa316aa2483f73bfaebfc36ccc4
harshp8513 <harshp8513@gmail.com>
2018-05-23 19:12:50 +0530

    http://localhost:816/allocateprojects
    solve bug on http://localhost:816/allocateprojects

--- app/Http/Controllers/AllocateProjectsController.php
+++ app/Http/Controllers/AllocateProjectsController.php
@@ -57,7 +57,7 @@ class AllocateProjectsController extends Controller
                 ['project_id', '=', $projname[$i]],
             ])->count();
             if($proj_count==0) DB::table('allocation')->insert([
-                ['user_id' => $userid, 'project_id'=> $projname[$i]],
+                ['user_id' => $userid, 'project_id'=> $projname[$i],'is_delete'=>0],
             ]);
         }
 

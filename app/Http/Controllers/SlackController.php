46bd4531dcc33e35008bcadcbc219f3d16f9ead9
Unknown <jangjinming311@outlook.com>
2018-05-31 11:06:22 +0700

    update slack state page and fix wordspace_id

--- app/Http/Controllers/SlackController.php
+++ app/Http/Controllers/SlackController.php
@@ -56,7 +56,7 @@ class SlackController extends Controller
             }])
             ->where('slack_user_id', '<>', '')
                ->where('workspace_id', '<>', '')
-               ->where('level', '=',11)
+//               ->where('level', '=',11)
                ->where('type', '=',2)
                ->where('channel_id','<>', '')
                ->orderBy('workspace_id', 'asc')
--- database/migrations/2018_05_31_040051_modify_fieldword_users_table.php
+++ database/migrations/2018_05_31_040051_modify_fieldword_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DeleteTaskColumnFromProjectTable extends Migration
+class ModifyFieldwordUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class DeleteTaskColumnFromProjectTable extends Migration
      */
     public function up()
     {
-        Schema::table('project', function (Blueprint $table) {
-            $table->dropColumn('task');
+        Schema::table('users', function (Blueprint $table) {
+            $table->Integer('workspace_id')->nullable()->change();
         });
     }
 
@@ -25,8 +25,6 @@ class DeleteTaskColumnFromProjectTable extends Migration
      */
     public function down()
     {
-        Schema::table('project', function (Blueprint $table) {
-            $table->string('task', 255);
-        });
+        //
     }
 }

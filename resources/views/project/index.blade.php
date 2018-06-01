461f4b5a79cd52ae7471d5328607e7b2da87b41e
Unknown <jangjinming311@outlook.com>
2018-06-01 21:41:05 +0700

    Fix script order error

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -8,16 +8,16 @@
       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
           <div class="card">
               <div class="header">                
-                    <div class="col-md-1" style="position: absolute;left: 100px;top: 7px;">
-                        <select class="form-control show-tick">
-                            <option value="0">All</option>
-                            <option value="1">Upcoming</option>
-                            <option value="2" selected="selected">Live</option>
-                            <option value="3">Hold</option>
-                            <option value="4">Closed</option>
-                            <option value="5">Deleted</option>
-                        </select>
-                    </div>
+                <div class="col-md-1" style="position: absolute;left: 100px;top: 7px;">
+                    <select class="form-control show-tick">
+                        <option value="0">All</option>
+                        <option value="1">Upcoming</option>
+                        <option value="2" selected="selected">Live</option>
+                        <option value="3">Hold</option>
+                        <option value="4">Closed</option>
+                        <option value="5">Deleted</option>
+                    </select>
+                </div>
                 
                   <ul class="header-dropdown m-r--5" style="position: absolute;top: 7px;">
                       <li class="dropdown">
@@ -90,46 +90,49 @@
           </div>
       </div> 
   </div>
+@endsection
+@section('project-scripts')
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript">
-    var mytable;
-    $(document).ready(function(){
-        mytable = $("#DataTables_Table_0").DataTable();
-      $(".input-sm").val(100);  
-      $("ul.dropdown-menu li").click(function(){
-        status = $(this).text();
-        $.ajax({
-            type: "POST",
-            url: '/project/getfromstatus',
-            data: {status : status},
-            dataType: "JSON",
-            success: function(resp) {
+    <script type="text/javascript">
 
-                if (mytable) mytable.clear();
-              response = resp.string;
+        $(document).ready(function(){
 
-              var result = response.map(function(item){
-                  var result = [];
-                    result.push("<svg height='20' width='20' style='position: absolute;left: 50px;"
-                         + "margin-top: 5px;' ><circle cx='10' cy='10' r='8'  fill='"+item.hot+"'/></svg>");
-                    result.push(item.p_name);
-                    result.push(item.p_client);
-                    result.push(item.developer);
-                    result.push(item.task);
-                    result.push(item.level);
-                    result.push(item.status);
-                    result.push("<a href='/project/"+item.id+"/edit'>Edit</a>");
-                    result.push("");
-                    // .... add all the values required
-                  console.log(result);
-                    return result;
-                });
-                mytable.rows.add(result); // add to DataTable instance
-                mytable.draw(); // always redraw
-            }
-        })
-      })
-    });
-</script> 
+            //mytable = $("#project_datable").DataTable();
+
+            $(".input-sm").val(100);
+            $("ul.dropdown-menu li").click(function(){
+
+                status = $(this).text();
+                $.ajax({
+                    type: "POST",
+                    url: '/project/getfromstatus',
+                    data: {status : status},
+                    dataType: "JSON",
+                    success: function(resp) {
+                        if (mytable) mytable.clear();
+                        response = resp.string;
+
+                        var result = response.map(function(item){
+                            var result = [];
+                            result.push("<svg height='20' width='20' style='position: absolute;left: 50px;"
+                                + "margin-top: 5px;' ><circle cx='10' cy='10' r='8'  fill='"+item.hot+"'/></svg>");
+                            result.push(item.p_name);
+                            result.push(item.p_client);
+                            result.push(item.developer);
+                            result.push(item.task);
+                            result.push(item.level);
+                            result.push(item.status);
+                            result.push("<a href='/project/"+item.id+"/edit'>Edit</a>");
+                            result.push("");
+                            // .... add all the values required
+                            console.log(result);
+                            return result;
+                        });
+                        mytable.rows.add(result); // add to DataTable instance
+                        mytable.draw(); // always redraw
+                    }
+                })
+            })
+        });
+    </script>
 @endsection
